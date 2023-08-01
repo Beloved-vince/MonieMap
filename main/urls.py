63048abc68dbdfeacc88534dd55b25ddee1084c8
Beloved-vince <vinceoludare@gmail.com>
2023-08-01 13:14:02 +0100

    google chart

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("current_streak/", views.user_dashboard, name="streak"),
+    path('transaction/', views.transaction, name='transaction'),
+    path('history/', views.history, name='history'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def user_dashboard(request):
     return render(request, 'home.html')
+
+
+def history(request):
+    return render(request, 'history.html')
+
+
+def transaction(request):
+    return render(request, 'transaction.html')
