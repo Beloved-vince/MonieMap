32df4835389c894aba7fed95deb2b8f36cdf3113
Beloved-vince <vinceoludare@gmail.com>
2023-08-03 13:31:33 +0100

    quick fix

--- main/views.py
+++ main/views.py
@@ -1,23 +1,79 @@
 from django.shortcuts import render
 from .models import Transaction
 from .forms  import TransactionForm
-from django.http import JsonResponse
+import json
 # from accounts
 
+import json
+from decimal import Decimal
+from django.http import JsonResponse
+
+class DecimalJSONEncoder(json.JSONEncoder):
+    """ JSON Encoder """
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return str(obj)
+        return super().default(obj)
+
 def user_dashboard(request):
-    return render(request, 'home.html')
+
+    transactions = Transaction.objects.all()
+    data = [
+        {
+            'name': t.name,
+            'transactionType': t.transactionType,
+            'select_type': t.select_type,
+            'amount': str(t.amount),
+            'date': t.date.strftime('%Y-%m-%d %H:%M:%S')
+        }
+        for t in transactions
+    ]
+ 
+    #Calculate total income, total expenses, and balanace and return a json object
+    total_income = Decimal(0)
+    total_expenses = Decimal(0)
+    for datum in data:
+        if datum['transactionType'] == 'income':
+            total_income += Decimal(datum['amount'])
+        elif datum['transactionType'] == 'expense':
+            total_expenses += Decimal(datum['amount'])
+    
+    balance = total_income - total_expenses
+    
+    data_json = json.dumps(data, cls=DecimalJSONEncoder)
+    total_json = json.dumps({
+        'total_income': str(total_income),
+        'total_expenses': str(total_expenses),
+        'balance': str(balance)
+    })
+
+    return render(request, 'home.html', context={'total_json': total_json})
 
 
 def history(request):
     """
     Return transaction history to the view in a tabular form
     name: outgoing or incoming name
-    finance: income or expenses
-    category: income/expenses category type
+    transactionType: income or expenses
+    select_type: income/expenses category type
     date: date 
     amount: outgoing or incoming amount
     """
-    return render(request, 'history.html')
+    transactions = Transaction.objects.all()
+    data = [
+        {
+            'name': t.name,
+            'transactionType': t.transactionType,
+            'select_type': t.select_type,
+            'amount': str(t.amount),  # Convert Decimal to string
+            'date': t.date.strftime('%Y-%m-%d %H:%M:%S')
+        }
+        for t in transactions
+    ]
+
+    # Convert the data list to a JSON string using the custom encoder
+    data_json = json.dumps(data, cls=DecimalJSONEncoder)
+    return render(request, 'history.html', context={'data_json': data_json})
 
 
 def transaction(request):
@@ -32,10 +88,3 @@ def transaction(request):
         form =TransactionForm()
         
     return render(request, 'transaction.html', {'form': form})
-
-
-def get_transactions(request):
-    transactions = Transaction.objects.all()
-    data = [{'name': t.name, 'transactionType': t.transactionType, 'select_type': t.select_type,
-             'amount': t.amount, 'date': t.date.strftime('%Y-%m-%d %H:%M:%S')} for t in transactions]
-    return JsonResponse(data, safe=False)
